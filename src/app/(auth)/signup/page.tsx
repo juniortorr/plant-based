41e410683c356a4f7c867ea527ed9875b32e3ad4
juniortorr <juniortorr77@gmail.com>
2024-03-08 22:56:36 -0800

    complete sign up form validations and refactor code in sign up server action

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,19 +2,39 @@
 import { handleAddUser } from '../../actions';
 import { useFormState } from 'react-dom';
 
+const initial = {
+  email: null,
+  password: null,
+};
+
 export default function SignUp() {
-  const [state, formAction] = useFormState(handleAddUser, null);
+  const [state, formAction] = useFormState(handleAddUser, initial);
   return (
     <>
       <h1>Sign Up Here</h1>
       <form action={formAction}>
         <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email" required />
+        <input type="email" id="email" name="email" required />
         <label htmlFor="password">Password:</label>
-        <input type="text" id="password" name="password" required />
+        <input type="text" id="password" minLength={8} name="password" required />
         <button>Create Account</button>
-        {state && <p>{state}</p>}
+        {state.password && (
+          <p>
+            password must be 8 characters long, contain at least 1 uppercase, and contain one
+            special character
+          </p>
+        )}
+        {state.email && <p>A user with this account already exists!</p>}
       </form>
     </>
   );
 }
+
+// List of validations
+// Email:
+// Empty String: Validated with required
+// Correct format: Validated with Type attribute
+// Password:
+// Must be a min-length 8 characters
+// Must contain one capital
+// Create Validation tests
--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,28 +1,41 @@
 'use server';
-
+import passwordValidation from './lib/validators';
 import { revalidatePath } from 'next/cache';
 import { v4 as uuidv4 } from 'uuid';
 import connectDB from '../../config/db';
 import Users from './lib/models';
 import { notFound, redirect } from 'next/navigation';
+import { isNull } from 'util';
 const bcrypt = require('bcrypt');
 const salt = 10;
 
 export async function handleAddUser(prevState: any, formData: FormData) {
   await connectDB();
-  const password = formData.get('password');
-  const email = formData.get('email');
-  const uniqueId = uuidv4();
-  const hashedPw = await bcrypt.hash(password, salt);
-  const credentials = { id: uniqueId, email: email, password: hashedPw };
-  const existingUser = await Users.findOne({ email: email });
+  const err = {
+    email: null,
+    password: null,
+  };
+  const credentials = {
+    id: uuidv4(),
+    email: formData.get('email'),
+    password: formData.get('password'),
+  };
+  if (passwordValidation(credentials.password) === false) {
+    err.password = true;
+    return err;
+  }
+  const existingUser = await Users.findOne({ email: credentials.email });
   if (!existingUser) {
+    const hashedPw = await bcrypt.hash(credentials.password, salt);
+    credentials.password = hashedPw;
     await Users.create(credentials);
     console.log('User successfully created!');
     revalidatePath('/signup');
     redirect('/login');
+  } else {
+    err.email = true;
+    return err;
   }
-  return 'There is already a user with this email!';
 }
 
 export async function handleLogin(formData) {
