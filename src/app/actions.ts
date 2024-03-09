533f929cef81c1ba9f7d2eefd356abbcd909988b
juniortorr <juniortorr77@gmail.com>
2024-03-08 22:57:00 -0800

    remove unused code

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,8 +4,7 @@ import { revalidatePath } from 'next/cache';
 import { v4 as uuidv4 } from 'uuid';
 import connectDB from '../../config/db';
 import Users from './lib/models';
-import { notFound, redirect } from 'next/navigation';
-import { isNull } from 'util';
+import { redirect } from 'next/navigation';
 const bcrypt = require('bcrypt');
 const salt = 10;
 
