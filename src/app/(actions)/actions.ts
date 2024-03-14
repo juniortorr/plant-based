9208384afd24709723f3cc803d9e14ec5e3031c7
juniortorr <juniortorr77@gmail.com>
2024-03-13 23:07:24 -0700

    add in admin authentication aand separate admin jwt

--- src/app/(actions)/actions.ts
+++ src/app/(actions)/actions.ts
@@ -4,11 +4,12 @@ import passwordValidation from '../lib/validators';
 import { v4 as uuidv4 } from 'uuid';
 import connectDB from '../../../config/db';
 import Users from '../lib/models';
-import { redirect } from 'next/navigation';
+import { RedirectType, redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
 import { SignJWT } from 'jose';
 import seedDb from '../lib/seedBlogs';
 import { userSchema } from '../lib/validators';
+import { getCredentials, initializeErrors } from '../lib/credentials-helpers';
 
 const bcrypt = require('bcrypt');
 const salt = 10;
@@ -17,22 +18,12 @@ export async function handleAddUser(prevState: any, formData: FormData) {
   let confirmUser = false;
   try {
     await connectDB();
-    const err = {
-      email: null,
-      password: null,
-      zod: null,
-    };
-    const credentials = {
-      id: uuidv4(),
-      firstName: formData.get('firstName'),
-      lastName: formData.get('lastName'),
-      email: formData.get('email'),
-      password: formData.get('password'),
-      savedBlogs: [],
-    };
+    const err = initializeErrors();
+    const credentials = getCredentials(formData);
     const result: any = userSchema.safeParse(credentials);
     if (!result.success) {
       err.zod = result.error.errors[0].message;
+      console.log(result.error.errors);
       return err;
     }
     if (passwordValidation(credentials.password) === false) {
@@ -41,8 +32,7 @@ export async function handleAddUser(prevState: any, formData: FormData) {
     }
     const existingUser = await Users.findOne({ email: credentials.email });
     if (!existingUser) {
-      const hashedPw = await bcrypt.hash(credentials.password, salt);
-      credentials.password = hashedPw;
+      credentials.password = await bcrypt.hash(credentials.password, salt);
       await Users.create(credentials);
       confirmUser = true;
       console.log('User successfully created!');
@@ -59,6 +49,7 @@ export async function handleAddUser(prevState: any, formData: FormData) {
 }
 
 export async function handleLogin(prevState, formData: FormData) {
+  let userStatus: string;
   try {
     await connectDB();
     const credentials = { email: formData.get('email'), password: formData.get('password') };
@@ -70,27 +61,51 @@ export async function handleLogin(prevState, formData: FormData) {
     if (!isAuthenticated) {
       return 'Incorrect Password';
     }
-    const secret = new TextEncoder().encode(process.env.SECRET);
+
+    const clientSecret = new TextEncoder().encode(process.env.SECRET);
+    const adminSecret = new TextEncoder().encode(process.env.ADMIN_SECRET);
     const oneDay = 24 * 60 * 60 * 1000;
     const alg = 'HS256';
     const userInfo = {
       id: existingUser.id,
       email: existingUser.email,
       firstName: existingUser.firstName,
+      admin: existingUser.admin,
     };
-    const jwt = await new SignJWT(userInfo).setProtectedHeader({ alg }).sign(secret);
+    const jwt = await new SignJWT(userInfo).setProtectedHeader({ alg }).sign(clientSecret);
     cookies().set({ name: 'auth', value: jwt, expires: Date.now() + oneDay, httpOnly: true });
-    redirect('/profile');
+    if (userInfo.admin === true) {
+      userStatus = 'admin';
+      const jwt = await new SignJWT(userInfo).setProtectedHeader({ alg }).sign(adminSecret);
+      cookies().set({
+        name: 'admin',
+        value: jwt,
+        expires: Date.now() + oneDay,
+        httpOnly: true,
+      });
+    }
   } catch (e) {
     console.log(e);
+  }
+  if (userStatus === 'admin') {
+    console.log('hello there');
+    redirect('../admin');
+  } else {
+    redirect('/profile');
   }
 }
 
 export async function handleLogout() {
-  const cookie = cookies().get('auth');
-  if (cookie) {
+  const clientCookie = cookies().get('auth');
+  const adminCookie = cookies().get('admin');
+
+  if (clientCookie) {
     cookies().delete('auth');
-    console.log('logged out successfully');
+    console.log('admin logged out successfully');
+  }
+  if (adminCookie) {
+    cookies().delete('admin');
+    console.log('admin logged out successfully');
   }
   redirect('/login');
 }
