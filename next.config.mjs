7cbf66f6f0b12a32f33bb67d0c1808f1225e042e
juniortorr <juniortorr77@gmail.com>
2024-03-19 15:38:18 -0700

    comment out unused code

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['i.annihil.us'],
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['i.annihil.us'],
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
