28862604a48ef927cb95dcb35d0a6c2a56de7c35
juniortorr <juniortorr77@gmail.com>
2024-03-19 21:41:58 -0700

    style blogs page

--- src/app/(explore)/blogs/page.tsx
+++ src/app/(explore)/blogs/page.tsx
@@ -1,27 +1,56 @@
 import connectDB from 'config/db';
 import BlogPosts from '../../lib/blogModel';
 import Link from 'next/link';
+import SharedHeader from 'src/app/components/Header-Shared';
+import BlogCard from 'src/app/components/Blog-Card';
+import { cookies } from 'next/headers';
+import { decryptJWT } from 'src/app/lib/auth';
+import Users from '@/models';
+import { revalidatePath } from 'next/cache';
+import { saveBlog } from 'src/app/(actions)/actions';
 
 export default async function Blogs() {
   await connectDB();
-  const posts = await BlogPosts.find();
-  console.log(posts);
+  const auth = cookies().get('auth');
+  const blogs = await BlogPosts.find();
+  const posts = await JSON.parse(JSON.stringify(blogs));
   return (
     <>
-      {posts.map((blog) => {
-        // if (blog.private === true) {
-        //   return <h1 key={blog.id}>Private</h1>;
-        // }
-        return (
-          <div key={blog.id}>
-            <p>{blog.title}</p>
-            <p>{blog.body}</p>
-            <button>
-              <Link href={{ pathname: `/blogs/${blog.id}` }}>Read Blog</Link>
-            </button>
-          </div>
-        );
-      })}
+      <SharedHeader title={'Blogs'} />
+      <main className="mx-auto mt-5 flex flex-wrap justify-center gap-3">
+        {posts.map((blog) => {
+          // if (blog.private === true) {
+          //   return <h1 key={blog.id}>Private</h1>;
+          // }
+          return (
+            <div
+              className="group flex size-64 flex-col justify-center gap-2 bg-accent/50 text-center"
+              key={blog.id}
+            >
+              <p className="text-xl font-bold group-hover:hidden">{blog.title}</p>
+              <button className="mx-auto hidden h-10 w-4/5 bg-green text-lg font-semibold text-white group-hover:block">
+                <Link href={{ pathname: `/blogs/${blog.id}` }}>Read Blog</Link>
+              </button>
+              {auth && (
+                <form
+                  action={async () => {
+                    'use server';
+                    saveBlog(blog);
+                  }}
+                >
+                  <button className="mx-auto hidden h-10 w-4/5 bg-green text-lg font-semibold text-white group-hover:block">
+                    Save Blog
+                  </button>
+                </form>
+              )}
+            </div>
+          );
+        })}
+      </main>
     </>
   );
 }
+
+// we would need to decode the information
+// we would have to find the user
+// push the object ID onto the saved blogs
