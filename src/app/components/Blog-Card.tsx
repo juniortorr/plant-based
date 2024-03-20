b97c84af9afce2dd617d6a625ef94aed8666d9ff
juniortorr <juniortorr77@gmail.com>
2024-03-19 21:43:25 -0700

    protect admin actions on pubic card

--- src/app/components/Blog-Card.tsx
+++ src/app/components/Blog-Card.tsx
@@ -1,12 +1,15 @@
 import DeleteBtn from './Delete-Blog-Btn';
 import Link from 'next/link';
+import { cookies } from 'next/headers';
 
 interface BlogCardProps {
   title: string;
   id: string;
+  blog?: any;
 }
 
-const BlogCard = async ({ title, id }: BlogCardProps) => {
+const BlogCard = async ({ title, id, blog }: BlogCardProps) => {
+  const auth = cookies().get('admin');
   return (
     <article className="group relative flex size-60 max-w-60 cursor-pointer flex-col items-center justify-center bg-accent/50 p-6 text-center transition duration-300 ease-in-out hover:bg-accent/100 hover:text-white">
       <h3 className="text-xl font-semibold group-hover:hidden">{title}</h3>
@@ -15,13 +18,15 @@ const BlogCard = async ({ title, id }: BlogCardProps) => {
           View Post
         </button>
       </Link>
-      <Link href={`/blogs/edit/${id}`}>
-        <button className="hidden h-11 w-40 border-2 bg-white/0 text-lg text-white transition duration-300 ease-in-out hover:border-0 hover:bg-green/100 group-hover:block">
-          Edit Post
-        </button>
-      </Link>
+      {auth && (
+        <Link href={`/blogs/edit/${id}`}>
+          <button className="hidden h-11 w-40 border-2 bg-white/0 text-lg text-white transition duration-300 ease-in-out hover:border-0 hover:bg-green/100 group-hover:block">
+            Edit Post
+          </button>
+        </Link>
+      )}
 
-      <DeleteBtn id={id} />
+      <DeleteBtn id={id} blog={blog} />
     </article>
   );
 };
--- src/app/components/Delete-Blog-Btn.tsx
+++ src/app/components/Delete-Blog-Btn.tsx
@@ -1,21 +1,32 @@
 import Image from 'next/image';
 import { handleDeleteBlog } from '../(actions)/admin-actions';
 import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+import { handleDeleteClient } from '../(actions)/actions';
 
 interface DeleteBtnProps {
   id: string;
+  blog?: any;
 }
 
-const DeleteBtn = async ({ id }: DeleteBtnProps) => {
-  const deleteBlog = async () => {
+const DeleteBtn = async ({ id, blog }: DeleteBtnProps) => {
+  const parsedBlog = JSON.parse(JSON.stringify(blog));
+
+  const handleDeleteOptions = async () => {
     'use server';
-    await handleDeleteBlog(id);
+    const admin = cookies().get('admin');
+    if (admin) {
+      await handleDeleteBlog(id);
+    } else {
+      await handleDeleteClient(parsedBlog);
+    }
     revalidatePath('/admin');
   };
+
   return (
     <form
       className="absolute -right-4 -top-4 hidden transition duration-300 ease-in-out group-hover:block"
-      action={deleteBlog}
+      action={handleDeleteOptions}
     >
       <button>
         <Image
