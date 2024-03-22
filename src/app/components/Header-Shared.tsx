819662944d0d621e7efe71f49b7af8ce71a1b450
juniortorr <juniortorr77@gmail.com>
2024-03-21 20:50:30 -0700

    adjust class order

--- src/app/components/Header-Shared.tsx
+++ src/app/components/Header-Shared.tsx
@@ -4,7 +4,7 @@ export default async function SharedHeader({ title }) {
   return (
     <header className="flex h-1/3 justify-center bg-trees-background bg-cover bg-center bg-no-repeat px-6">
       <Nav color="text-black"></Nav>
-      <h1 className=" flex h-1/2 items-center text-center text-3xl font-bold mt-3">{title}</h1>
+      <h1 className=" mt-3 flex h-1/2 items-center text-center text-3xl font-bold">{title}</h1>
     </header>
   );
 }
