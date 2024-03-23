c7a61b562bce7c702d71e35f25d714e08d3ce6c2
juniortorr <juniortorr77@gmail.com>
2024-03-23 10:51:33 -0700

    adjust sizing of footer to fit mobile & be responsive

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link';
 
 const Footer = async () => {
   return (
-    <footer className="flex flex-col items-center justify-center gap-6 bg-green py-10 text-white">
+    <footer className="flex flex-col items-center justify-center gap-6 bg-green px-2 py-10 text-white">
       <p className="text-center leading-5">
         Subcribe to receive <br></br> updates on new content
       </p>
-      <div className="flex items-center gap-2">
+      <div className="flex flex-wrap items-center justify-center gap-2">
         <label htmlFor="email" className="text-lg">
           Email:
-          <input type="text" id="email" name="email" className="ml-3" />
         </label>
+        <input type="text" id="email" name="email" />
         <button className="bg-accent px-4 py-1">Sign Up</button>
       </div>
       <div className="flex w-24 flex-wrap justify-center gap-1 italic ">
