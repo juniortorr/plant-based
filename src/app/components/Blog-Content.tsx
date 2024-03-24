037bf0d968177366f957a03451db26928246a03a
juniortorr <juniortorr77@gmail.com>
2024-03-24 13:58:47 -0700

    blog page desktop media queries

--- src/app/components/Blog-Content.tsx
+++ src/app/components/Blog-Content.tsx
@@ -1,10 +1,10 @@
 export default async function BlogContent({ section }) {
   return (
     <section className="flex flex-col items-center">
-      <h2 className="py-8 text-center text-xl font-bold">{section.subheading}</h2>
+      <h2 className="py-8 text-center text-xl font-bold lg:text-2xl">{section.subheading}</h2>
       {section.paragraphs.map((p, index) => {
         return (
-          <p key={index} className="mb-2 max-w-2xl px-10 text-left indent-3 last:mb-0">
+          <p key={index} className="mb-2 max-w-2xl px-10 text-left indent-3 text-lg last:mb-0">
             {p}
           </p>
         );
